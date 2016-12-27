476095181f5cb64cec3c9f61b67e08bf1ad2cc8b
Jonathan Cabrera <akounanagmahal@gmail.com>
2016-12-27 19:04:28 +0900

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -2,8 +2,8 @@
 require(ggplot2)
 
 # Loading provided datasets - loading from local machine
-NEI <- readRDS("~/Exploratory_Data_Analysis/Assignment_2/summarySCC_PM25.rds")
-SCC <- readRDS("~/Exploratory_Data_Analysis/Assignment_2/Source_Classification_Code.rds")
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
 
 # Sampling
 NEI_sampling <- NEI[sample(nrow(NEI), size=5000, replace=F), ]
@@ -13,12 +13,12 @@ MD <- subset(NEI, fips == 24510)
 MD$year <- factor(MD$year, levels=c('1999', '2002', '2005', '2008'))
 
 # Of the four types of sources indicated by the type (point, nonpoint, onroad, nonroad) variable, 
-# which of these four sources have seen decreases in emissions from 1999–2008 for Baltimore City? 
-# Which have seen increases in emissions from 1999–2008? 
+# which of these four sources have seen decreases in emissions from 1999-2008 for Baltimore City? 
+# Which have seen increases in emissions from 1999-2008? 
 # Use the ggplot2 plotting system to make a plot answer this question.
 
 # Generate the graph in the same directory as the source code
-png(filename='~/Exploratory_Data_Analysis/Assignment_2/plot3.png', width=800, height=500, units='px')
+png(filename='plot3.png', width=800, height=500, units='px')
 
 ggplot(data=MD, aes(x=year, y=log(Emissions))) + facet_grid(. ~ type) + guides(fill=F) +
     geom_boxplot(aes(fill=type)) + stat_boxplot(geom ='errorbar') +
