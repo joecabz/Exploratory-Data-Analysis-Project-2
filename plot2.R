e28179ef1befda9284dc37e4e0cca51a45bb126e
Jonathan Cabrera <akounanagmahal@gmail.com>
2016-12-27 19:00:50 +0900

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,6 +1,6 @@
 # Loading provided datasets - loading from local machine
-NEI <- readRDS("~/Exploratory_Data_Analysis/Assignment_2/summarySCC_PM25.rds")
-SCC <- readRDS("~/Exploratory_Data_Analysis/Assignment_2/Source_Classification_Code.rds")
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
 
 # Sampling
 NEI_sampling <- NEI[sample(nrow(NEI), size=5000, replace=F), ]
@@ -12,7 +12,7 @@ MD <- subset(NEI, fips=='24510')
 # from 1999 to 2008? Use the base plotting system to make a plot answering this question.
 
 # Generate the graph in the same directory as the source code
-png(filename='~/Exploratory_Data_Analysis/Assignment_2/plot2.png')
+png(filename='plot2.png')
 
 barplot(tapply(X=MD$Emissions, INDEX=MD$year, FUN=sum), 
         main='Total Emission in Baltimore City, MD', 
