ade8558649983ee3623dbdaeea2be916c6afac98
Jonathan Cabrera <akounanagmahal@gmail.com>
2016-12-27 18:57:59 +0900

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,7 +1,6 @@
-
 # Loading provided datasets - loading from local machine
-NEI <- readRDS("~/Exploratory_Data_Analysis/Assignment_2/summarySCC_PM25.rds")
-SCC <- readRDS("~/Exploratory_Data_Analysis/Assignment_2/Source_Classification_Code.rds")
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
 
 # Sampling
 NEI_sampling <- NEI[sample(nrow(NEI), size=2000, replace=F), ]
@@ -15,7 +14,7 @@ Emissions$PM <- round(Emissions[,2]/1000,2)
 # for each of the years 1999, 2002, 2005, and 2008.
 
 # Generate the graph in the same directory as the source code
-png(filename='~/Exploratory_Data_Analysis/Assignment_2/plot1.png')
+png(filename='plot1.png')
 
 barplot(Emissions$PM, names.arg=Emissions$Group.1, 
         main=expression('Total Emission of PM'[2.5]),
